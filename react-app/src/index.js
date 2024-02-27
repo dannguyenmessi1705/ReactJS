8d6b388ecd0c62f06348510f883b6d8df6a28ae3
dannguyenmessi1705 <messiprohy@gmail.com>
2024-02-27 11:26:34 +0700

    Rename Folder

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './PropState/PropsState.css';
-import PropsState from './PropState/PropsState';
-import './UseEffect/UseEffect.css';
-import UseEffect from './UseEffect/UseEffect';
-import ConvertCurrency from './UseEffect/ConvertCurrency';
-import './UseEffect/ConverCurrency.css';
-import CustomHook from './CustomHook/CustomHook';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./00.PropState/PropsState.css";
+import PropsState from "./00.PropState/PropsState";
+import "./01.UseEffect/UseEffect.css";
+import UseEffect from "./01.UseEffect/UseEffect";
+import ConvertCurrency from "./01.UseEffect/ConvertCurrency";
+import "./01.UseEffect/ConverCurrency.css";
+import CustomHook from "./02.CustomHook/CustomHook";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // StrictMode là một công cụ để kiểm tra các warning và cảnh báo trong ứng dụng React (nhưng không phải là một công cụ để kiểm tra lỗi)
   // Thường bỏ đi khi ứng dụng được triển khai (production), chỉ dùng trong quá trình phát triển (development)
   //  {/* <PropsState /> */} {/* Props và State */}
-    // <UseEffect /> // {/* useEffect */}
-    // <ConvertCurrency />  // {/* useEffect */}
-    <CustomHook /> // {/* Custom Hook */}
-
+  // <UseEffect /> // {/* useEffect */}
+  // <ConvertCurrency />  // {/* useEffect */}
+  <CustomHook /> // {/* Custom Hook */}
 );
