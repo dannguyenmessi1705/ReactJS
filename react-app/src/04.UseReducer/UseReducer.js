c7b0d40aa76f1f4f5efbc8d6058c3f59a6db465d
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-02 16:32:58 +0700

    Add Loader, Error, and StartScreen components

--- react-app/src/04.UseReducer/UseReducer.js
+++ react-app/src/04.UseReducer/UseReducer.js
@@ -2,47 +2,52 @@ import React, { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initState = {
   questions: [],
-  status: "loading" // status gán là "loading", "ready", "error", "active" tùy thuộc vào trạng thái của ứng dụng
+  status: "loading", // status gán là "loading", "ready", "error", "active" tùy thuộc vào trạng thái của ứng dụng
 }; // Đây là state khởi tạo
 
 const reducer = (state, action) => {
-  switch (action.type){
+  switch (action.type) {
     case "dataReceived": // Nếu action.type là "dataReceived"
-      return {...state, questions: action.payload, status: "ready"}; // Trả về state mới
+      return { ...state, questions: action.payload, status: "ready" }; // Trả về state mới
     case "dataFailed": // Nếu action.type là "dataFailed"
-      return {...state, status: "error"}; // Trả về state mới
+      return { ...state, status: "error" }; // Trả về state mới
     default:
       throw new Error("Unkwown action"); // Nếu action.type không khớp với bất kỳ case nào thì throw error
-  }  
+  }
 }; // Đây là reducer
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initState); // Khai báo state và dispatch từ useReducer, reducer và initState
+  const { questions, status } = state; // Destructuring state thành questions và status
   useEffect(() => {
     const loadQuestion = async () => {
       try {
         const res = await fetch("http://localhost:8175/questions");
         if (!res.ok) throw new Error("Error to fetch"); // Nếu fetch không thành công thì throw error
         const data = await res.json(); // Nếu fetch thành công thì parse data từ json
-        dispatch({type: "dataReceived", payload: data}); // Dispatch action "dataReceived" và truyền data vào payload
-      } catch (err){
-        dispatch({type: "dataFailed"}); // Nếu fetch không thành công thì dispatch action "dataFailed"
+        dispatch({ type: "dataReceived", payload: data }); // Dispatch action "dataReceived" và truyền data vào payload
+      } catch (err) {
+        dispatch({ type: "dataFailed" }); // Nếu fetch không thành công thì dispatch action "dataFailed"
       }
-    }
+    };
     loadQuestion();
-  }, []) // Sử dụng useEffect để fetch data từ server và dispatch action tương ứng
+  }, []); // Sử dụng useEffect để fetch data từ server và dispatch action tương ứng
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>Questtion</p>
-        <p>1/15</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={questions.length}/>}
       </Main>
     </div>
-  )
+  );
 };
 
 export default App;
