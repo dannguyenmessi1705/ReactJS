b8943c7c7125c9435e00ca075e4419d01a162bdf
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-05 21:06:23 +0700

    Use Router, Routes, BrowserRouter in react-router-dom

---
+++ vite-app/src/SinglePageApp/Example/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from "./page/Home"
+import Price from "./page/Price"
+import Product from "./page/Product"
+import NotFound from "./page/NotFound"
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/product' element={<Product />} />
+                <Route path='/price' element={<Price />}/>
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
