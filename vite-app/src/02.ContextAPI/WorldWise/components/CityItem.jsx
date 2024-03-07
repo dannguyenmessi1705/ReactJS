96a0663a89c70b4549cad47d12875deab0693259
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-07 10:09:16 +0700

    Add useCity hook to CityItem component to show current City

--- vite-app/src/02.ContextAPI/WorldWise/components/CityItem.jsx
+++ vite-app/src/02.ContextAPI/WorldWise/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCity } from "../contexts/CitiesContext";
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -7,11 +8,14 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 function CityItem({ city }) {
+  const { currentCity } = useCity(); // Sử dụng hook useCity để lấy giá trị từ context
   return (
     <li>
       <Link
         to={`${city.id}?lat=${city.position.lat}&lng=${city.position.lng}`} // Tạo đường dẫn động (params là id, query là lat và lng)
-        className={styles.cityItem}
+        className={`${styles.cityItem} 
+        ${city.id === currentCity.id ? styles["cityItem--active"] : ""}
+        }`} // Nếu id của city hiện tại trùng với id của city đang xét thì thêm class cityItem--active
       >
         <span className={styles.emoji}>{city.emoji}</span>
         <h3 className={styles.name}>{city.cityName}</h3>
