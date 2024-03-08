1a2931250c8a577bb0de45ab63d59ffbfc321f8e
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-08 23:59:33 +0700

    Display all City marked in the Leaflet Map

--- vite-app/src/02.ContextAPI/WorldWise/components/Map.jsx
+++ vite-app/src/02.ContextAPI/WorldWise/components/Map.jsx
@@ -2,15 +2,17 @@ import { useParams, useSearchParams, useNavigate } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
 import styles from "./Map.module.css";
+import { useCity } from "../contexts/CitiesContext";
 function Map() {
   const [mapPos, setMapPos] = useState([0, 0]); // Lưu vị trí của map
+  const { cities } = useCity(); // Lấy danh sách city từ context
   const navigate = useNavigate(); // Hàm này giúp chuyển hướng trang
   const params = useParams(); // Lấy params từ đường dẫn
   const [searchParams, setSearchParams] = useSearchParams(); // Lấy query từ đường dẫn
   return (
     <div className={styles.mapContainer}>
       <MapContainer
-        center={mapPos}
+        center={[0, 0]}
         zoom={13}
         scrollWheelZoom={true}
         className={styles.map}
@@ -19,11 +21,18 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => {
+          return (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={city.id}
+            >
+              <Popup>
+                <span>{city.emoji}</span> <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
