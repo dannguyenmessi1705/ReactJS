641547b13c8acd3916859f67919fb2df6376264a
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-14 21:21:52 +0700

    Thunks in Redux Toolkit

--- react-app/src/06.Redux/features/accounts/accountSlice.js
+++ react-app/src/06.Redux/features/accounts/accountSlice.js
@@ -96,21 +96,26 @@ const initialState = {
   isLoading: false,
 }; // state mặc định của ứng dụng
 
- // Tạo ra một slice với tên là account, state mặc định là initialState, và các reducers là các hàm thay đổi state
+// Tạo ra một slice với tên là account, state mặc định là initialState, và các reducers là các hàm thay đổi state
 const accountReducer = createSlice({
   name: "account", // Tên của slice
   initialState: initialState, // State mặc định của slice
-  reducers: { // Các reducers là các hàm thay đổi state
-    deposit: (state, action) => { // Hàm thay đổi state khi thực hiện action deposit (type: "account/deposit" vì redux toolkit tự động tạo ra type từ tên của slice và tên của reducer)
+  reducers: {
+    // Các reducers là các hàm thay đổi state
+    deposit: (state, action) => {
+      // Hàm thay đổi state khi thực hiện action deposit (type: "account/deposit" vì redux toolkit tự động tạo ra type từ tên của slice và tên của reducer)
       state.balance += action.payload;
       state.isLoading = false;
     },
-    withdraw: (state, action) => { // Hàm thay đổi state khi thực hiện action withdraw (type: "account/withdraw")
+    withdraw: (state, action) => {
+      // Hàm thay đổi state khi thực hiện action withdraw (type: "account/withdraw")
       if (state.balance < action.payload) return;
       state.balance -= action.payload;
     },
-    requestLoan: { // Hàm thay đổi state khi thực hiện action requestLoan (type: "account/requestLoan")
-      prepare: (amount, loanPurpose) => { // Hàm prepare trả về một object chứa payload của action
+    requestLoan: {
+      // Hàm thay đổi state khi thực hiện action requestLoan (type: "account/requestLoan")
+      prepare: (amount, loanPurpose) => {
+        // Hàm prepare trả về một object chứa payload của action
         return {
           payload: {
             amount,
@@ -118,14 +123,16 @@ const accountReducer = createSlice({
           },
         };
       },
-      reducer: (state, action) => { // Hàm reducer thực hiện thay đổi state
+      reducer: (state, action) => {
+        // Hàm reducer thực hiện thay đổi state
         if (state.loan > 0) return;
         state.balance += action.payload.amount;
         state.loanPurpose = action.payload.loanPurpose;
         state.loan = action.payload.amount;
       },
     },
-    payLoan: (state, action) => { // Hàm thay đổi state khi thực hiện action payLoan (type: "account/payLoan")
+    payLoan: (state, action) => {
+      // Hàm thay đổi state khi thực hiện action payLoan (type: "account/payLoan")
       if (state.balance < state.loan) return;
       state.balance -= state.loan;
       state.loan = 0;
@@ -134,6 +141,29 @@ const accountReducer = createSlice({
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } =
-  accountReducer.actions; // Export các action creator từ slice account để sử dụng trong component
+export const { withdraw, requestLoan, payLoan } = accountReducer.actions; // Export các action creator từ slice account để sử dụng trong component
+
+// Tạo ra 1 thunk để thực hiện action deposit, thay vì lấy deposit từ accountReducer.actions, ta sẽ tạo ra một hàm mới có tên là deposit
+export function deposit(amount, currency) {
+  if (currency === "USD") {
+    return {
+      type: "account/deposit", // Redux toolkit tự động tạo ra 1 action type từ tên của slice và tên của reducer, để gọi tới reducer deposit của slice account
+      payload: amount,
+    };
+  }
+  // Trả về một hàm nhận vào dispatch để thực hiện các action không đồng bộ
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "account/convertCurrency" }); // Thực hiện action convertCurrency để báo cho store là đang thực hiện chuyển đổi tiền tệ
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+      ); // Gọi API để chuyển đổi tiền tệ
+      if (!res.ok) throw new Error("Failed to convert currency."); // Nếu gọi API không thành công thì throw error
+      const data = await res.json(); // Lấy dữ liệu từ API
+      dispatch({ type: "account/deposit", payload: data["rates"]["USD"] }); // Thực hiện action deposit với giá trị chuyển đổi từ tiền tệ sang USD sau khi gọi API thành công
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
 export default accountReducer.reducer; // Export reducer từ slice account để sử dụng trong store
