1cf5bfb1ec5c7dbf235909702fee525f217f105b
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-14 17:38:17 +0700

    Create Store by configureStore in @reactjs/toolkit

--- react-app/src/06.Redux/store.js
+++ react-app/src/06.Redux/store.js
@@ -1,3 +1,4 @@
+/* CLASSIC REDUX
 import { createStore, combineReducers, applyMiddleware } from "redux";
 // createStore: Hàm tạo store
 // combineReducers: Hàm kết hợp nhiều reducer thành một reducer lớn
@@ -21,4 +22,16 @@ store.dispatch(withdraw(50)); // Thực hiện một action withdraw, redux sẽ
 store.dispatch(create("Alice", "123")); // Thực hiện một action create, redux sẽ gọi reducer để thay đổi state của customer
 store.dispatch(updateName("Bob")); // Thực hiện một action updateName, redux sẽ gọi reducer để thay đổi state của customer
 */
+
+/* MODERN REDUX (REDUX TOOLKIT) */
+import { configureStore } from "@reduxjs/toolkit"; // Import hàm configureStore từ thư viện @reduxjs/toolkit để tạo store
+import accountReducer from "./features/accounts/accountSlice"; // Nhập hàm reducer từ file accountSlice.js
+import customerReducer from "./features/customers/customerSlice"; // Nhập hàm reducer từ file customerSlice.js
+const store = configureStore({
+  reducer: {
+    // Truyền reducer vào store
+    account: accountReducer, // Tạo key account với giá trị là reducer accountReducer
+    customer: customerReducer, // Tạo key customer với giá trị là reducer customerReducer
+  },
+});
 export default store; // Xuất store để sử dụng trong ứng dụng
