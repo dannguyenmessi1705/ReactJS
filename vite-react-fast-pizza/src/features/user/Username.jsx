42635dc0a6ddeb0d2a09ceae913970de402b7e6a
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-18 01:22:39 +0700

    style: flex box in Tailwindcss

--- vite-react-fast-pizza/src/features/user/Username.jsx
+++ vite-react-fast-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Dan</div>;
+  return <div className="hidden text-sm font-semibold md:block">Dan</div>;
 }
 
 export default Username;
