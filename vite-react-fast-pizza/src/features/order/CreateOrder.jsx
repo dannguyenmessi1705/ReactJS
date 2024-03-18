5d8edc57367fdd1bf4aeb9157a7b885b03f344b8
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-19 01:31:15 +0700

    style: createOrder, title, svg

--- vite-react-fast-pizza/src/features/order/CreateOrder.jsx
+++ vite-react-fast-pizza/src/features/order/CreateOrder.jsx
@@ -43,30 +43,44 @@ function CreateOrder() {
   const isSubmitting = navigation.action === "submitting"; // Kiểm tra xem route có đang submit dữ liệu không
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="POST">
         {" "}
         {/* Tạo form với method là POST */}
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input w-full"
+          />
         </div>
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" required className="input w-full" />
+            {error?.phone && (
+              <div className="mt-2 rounded-full bg-red-100 p-2 text-xs text-red-700">
+                {error.phone}
+              </div>
+            )}
           </div>
-          {error?.phone && <div>{error.phone}</div>}
         </div>
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
           </div>
         </div>
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             type="checkbox"
             name="priority"
@@ -75,7 +89,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />{" "}
         {/* Tạo input hidden để chứa giỏ hàng, cần chuyển thành chuỗi JSON */}
