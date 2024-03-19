aa5bed4e891e45e47ab83dcbc2d11e7662aeeabc
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-20 00:17:09 +0700

    feat: modeling the username state with Redux toolkit

--- vite-react-fast-pizza/src/features/user/Username.jsx
+++ vite-react-fast-pizza/src/features/user/Username.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from "react-redux";
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Dan</div>;
+  const { username } = useSelector((store) => store.user);
+  if (!username) return;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
