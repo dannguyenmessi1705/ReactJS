d7dc5885d1436c7e0b336a6dca372d47a65d1a21
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-20 11:50:33 +0700

    feat: buliding redux in cartOverview

--- vite-react-fast-pizza/src/features/cart/CartOverview.jsx
+++ vite-react-fast-pizza/src/features/cart/CartOverview.jsx
@@ -1,10 +1,23 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { formatCurrency } from "../../utils/helpers";
 function CartOverview() {
+  const totalQuantity = useSelector((store) =>
+    store.cart.cart.reduce((sum, item) => {
+      return (sum += item.quantity);
+    }, 0),
+  );
+  const totalPrice = useSelector((store) =>
+    store.cart.cart.reduce((sum, item) => {
+      return (sum += item.totalPrice);
+    }, 0),
+  );
+  if (!totalQuantity) return null;
   return (
-    <div className="bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6 text-sm md:text-base flex items-center justify-between">
+    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 uppercase text-stone-200 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalQuantity} pizzas</span>
+        <span>{formatCurrency(totalPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
