577e06d8b64ebb4ceb37d851c000e33ac442e954
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-03-28 01:50:06 +0700

    fix: fix style component with element not in HTML (add $ in prefix element)

--- vite-react-wild-oasis/src/ui/Menus.jsx
+++ vite-react-wild-oasis/src/ui/Menus.jsx
@@ -37,8 +37,8 @@ const StyledList = styled.ul`
   box-shadow: var(--shadow-md);
   border-radius: var(--border-radius-md);
 
-  right: ${(props) => props.position.x}px;
-  top: ${(props) => props.position.y}px;
+  right: ${(props) => props.$position.x}px;
+  top: ${(props) => props.$position.y}px;
 `;
 
 const StyledButton = styled.button`
@@ -119,7 +119,7 @@ function List({ id, children }) {
 
   return createPortal(
     // Sử dụng createPortal để render menu ra ngoài root element
-    <StyledList position={position} ref={ref}>
+    <StyledList $position={position} ref={ref}>
       {" "}
       {/* Truyền position và ref vào StyledList */}
       {children}
